9d58f67cbba7630938cc1c1cdbf8c59bd5effbd6
LuanPonick <luanponick.sz.2@gmail.com>
2022-11-14 16:59:35 -0300

    paginaPrincipal updatte JS

--- js/main.js
+++ js/main.js
@@ -1,3 +1,31 @@
+class ONG{
+    constructor(Ongnome,foco,texto){
+        this.Ongnome = Ongnome;
+        this.foco = foco;
+        this.texto = texto;
+    }
+}
+var allOngs = [];
+
+allOngs.push(Ong01 = new ONG("texte" , "animais","Nos somos uma pequena ong relacionada a cudados animais de forma 100% gratuita e simplificada"));
+allOngs.push(Ong02 = new ONG("texte" , "animais","Nos somos uma pequena ong relacionada a cudados animais de forma 100% gratuita e simplificada"));
+allOngs.push(Ong03 = new ONG("texte" , "animais","Nos somos uma pequena ong relacionada a cudados animais de forma 100% gratuita e simplificada"));
+
+for(var i = 0 ; i < allOngs.length ;i++){
+    let divConteudoPrincipal;
+}
+
+
+
+
+
+
+
+
+
+
+
+
 function teste(){
     alert("reste");
 }
